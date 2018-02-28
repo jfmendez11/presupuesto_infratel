da8ddf382ddb85881c28a331418e99c6f6d3cdb6
jfmendez11 <jf.mendez11@uniandes.edu.co>
2018-02-28 14:35:28 -0500

    proyect.js ruteo

--- app/routes/api/proyectos.js
+++ app/routes/api/proyectos.js
@@ -152,7 +152,7 @@ module.exports = function (app, express) {
   apiRouter.route("/:id_proyecto/items/:id_item")
 
     .get(function (req, res) {
-      Item.findOne({ idProyecto: req.params.id_proyecto, _id: req.params.id_item },
+      Item.findById(req.params.id_item ,
         function (err, item) {
           if (err) res.send(err);
 
@@ -161,7 +161,7 @@ module.exports = function (app, express) {
     })
 
     .put(function (req, res) {
-      Item.findOne({ idProyecto: req.params.id_proyecto, _id: req.params.id_item },
+      Item.findById(req.params.id_item,
         function (err, item) {
           if (err) res.send(err);
 
@@ -188,8 +188,269 @@ module.exports = function (app, express) {
       });
     });
 
+    ////////////////////////CRUD para materia prima/////////////////////////////////////
+    //Materiales: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/materiales
+    apiRouter.route("/:id_proyecto/items/:id_item/materiales")
+    .post(function (req, res) {
+      var materialI = new Material();
+      materialI.valorUnit = req.body.valorUnit;
+      materialI.item = req.params.id_item;
+      materialI.material = req.body.material;
 
+      materialI.save(function (err) {
+        if (err) {
+          if (err.code == 11000)
+            return res.json({ success: false, message: "Ya existe un proyecto con este nombre." });
+          else
+            return res.send(err);
+        }
+        return res.json({ message: "Proyecto creado." });
+      });
+    })
+
+    .get(function (req, res) {
+      Material.find({}, function (err, materiales) {
+        if (err) res.send(err);
+        
+        res.json(materiales);
+      });
+    });
+
+    //Materiales: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/materiales/:id_material
+    apiRouter.route("/:id_proyecto/items/:id_item/materiales/:id_material")
+
+    .get(function(req, res) {
+      Material.findOne({item: req.params.id_item, _id: id_material}, 
+        function(err, material) {
+          if(err) res.send(err);
+
+          res.json(material);
+        }
+      );
+    })
     
+    .put(function (req, res) {
+      Material.findById(req.params.id_material, function (err, material) {
+        if(err) res.send(err);
+
+        if(req.body.valorUnit) material.valorUnit = req.body.valorUnit;
+
+        material.save(function (err) {
+          if (err) res.send(err);
+
+          res.json({ message: "Material actualizado." });
+        });
+      });
+    })
+
+    .delete(function (req, res) {
+      Material.remove({
+        _id: req.params.id_material,
+        idItem: req.params.id_item
+      }, function (err, material) {
+        if (err) res.send(err);
+
+        res.json({ message: "Material eliminado." });
+      });
+    });
+
+    //HyE: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/hyes
+    apiRouter.route("/:id_proyecto/items/:id_item/hyes")
+    .post(function (req, res) {
+      var hyeI = new HyE();
+      hyeI.valorUnit = req.body.valorUnit;
+      hyeI.item = req.params.id_item;
+      hyeI.material = req.body.material;
+    
+      hyeI.save(function (err) {
+        if (err) {
+          if (err.code == 11000)
+            return res.json({ success: false, message: "Ya existe un proyecto con este nombre." });
+          else
+            return res.send(err);
+        }
+        return res.json({ message: "Proyecto creado." });
+      });
+    })
+    
+    .get(function (req, res) {
+      HyE.find({}, function (err, hyes) {
+        if (err) res.send(err);
+            
+        res.json(hyes);
+      });
+    });
+    
+    //HyE: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/hyes/:id_hye
+    apiRouter.route("/:id_proyecto/items/:id_item/hyes/:id_hye")
+    
+    .get(function(req, res) {
+      HyE.findOne({item: req.params.id_item, _id: id_hye}, 
+        function(err, hye) {
+          if(err) res.send(err);
+    
+          res.json(hye);
+        }
+      );
+    })
+        
+    .put(function (req, res) {
+      HyE.findById(req.params.id_hye, function (err, hye) {
+        if(err) res.send(err);
+    
+        if(req.body.valorUnit) hye.valorUnit = req.body.valorUnit;
+        if(req.body.rendimiento) hye.rendimiento = req.body.rendimiento;
+    
+        hye.save(function (err) {
+          if (err) res.send(err);
+    
+          res.json({ message: "Herramienta o equipo actualizado." });
+        });
+      });
+    })
+    
+    .delete(function (req, res) {
+      HyE.remove({
+        _id: req.params.id_hye,
+        idItem: req.params.id_item
+      }, function (err, hye) {
+        if (err) res.send(err);
+    
+        res.json({ message: "Herramienta o equipo eliminado." });
+      });
+    });
+
+    //Trabajadores: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/trabajadores
+    apiRouter.route("/:id_proyecto/items/:id_item/trabajadores")
+    .post(function (req, res) {
+      var trabajadorI = new Mo();
+      trabajadorI.valorUnit = req.body.valorUnit;
+      trabajadorI.item = req.params.id_item;
+      trabajadorI.material = req.body.material;
+
+      trabajadorI.save(function (err) {
+        if (err) {
+          if (err.code == 11000)
+            return res.json({ success: false, message: "Ya existe un proyecto con este nombre." });
+          else
+            return res.send(err);
+        }
+        return res.json({ message: "Proyecto creado." });
+      });
+    })
+
+    .get(function (req, res) {
+      Mo.find({}, function (err, trabajadores) {
+        if (err) res.send(err);
+        
+        res.json(trabajadores);
+      });
+    });
+
+    //Trabajadores: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/trabajadores/:id_trabajador
+    apiRouter.route("/:id_proyecto/items/:id_item/trabajadores/:id_trabajador")
+
+    .get(function(req, res) {
+      Mo.findOne({item: req.params.id_item, _id: id_trabajador}, 
+        function(err, trabajador) {
+          if(err) res.send(err);
+
+          res.json(trabajador);
+        }
+      );
+    })
+    
+    .put(function (req, res) {
+      Mo.findById(req.params.id_trabajador, function (err, trabajador) {
+        if(err) res.send(err);
+
+        if(req.body.valorUnit) trabajador.valorUnit = req.body.valorUnit;
+        if(req.body.rendimiento) trabajador.rendimiento = req.body.valorUnit;
+
+        trabajador.save(function (err) {
+          if (err) res.send(err);
+
+          res.json({ message: "Trabajador actualizado." });
+        });
+      });
+    })
+
+    .delete(function (req, res) {
+      Mo.remove({
+        _id: req.params.id_trabajador,
+        idItem: req.params.id_item
+      }, function (err, trabajodr) {
+        if (err) res.send(err);
+
+        res.json({ message: "Trabajador eliminado." });
+      });
+    });
+
+    //Transportes: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/transportes
+    apiRouter.route("/:id_proyecto/items/:id_item/transportes")
+    .post(function (req, res) {
+      var transporteI = new Transporte();
+      transporteI.costoUnit = req.body.costoUnit;
+      transporteI.item = req.params.id_item;
+      transporteI.material = req.body.material;
+    
+      transporteI.save(function (err) {
+        if (err) {
+          if (err.code == 11000)
+            return res.json({ success: false, message: "Ya existe un proyecto con este nombre." });
+          else
+            return res.send(err);
+        }
+        return res.json({ message: "Proyecto creado." });
+      });
+    })
+    
+    .get(function (req, res) {
+      Transporte.find({}, function (err, transportes) {
+        if (err) res.send(err);
+            
+        res.json(transportes);
+      });
+    });
+    
+    //Transportes: http://localhost:8080/proyectos/:id_proyecto/items/:id_item/transportes/:id_transporte
+    apiRouter.route("/:id_proyecto/items/:id_item/transportes/:id_transporte")
+    
+    .get(function(req, res) {
+      Material.findOne({item: req.params.id_item, _id: id_transporte}, 
+        function(err, transporte) {
+          if(err) res.send(err);
+    
+          res.json(transporte);
+        }
+      );
+    })
+        
+    .put(function (req, res) {
+      Transporte.findById(req.params.id_transporte, function (err, transporte) {
+        if(err) res.send(err);
+    
+        if(req.body.valorUnit) transporte.valorUnit = req.body.valorUnit;
+        if(req.body.rendimiento) transporte.rendimiento = req.body.rendimiento;
+    
+        transporte.save(function (err) {
+          if (err) res.send(err);
+    
+          res.json({ message: "Transporte actualizado." });
+        });
+      });
+    })
+    
+    .delete(function (req, res) {
+      Transporte.remove({
+        _id: req.params.id_transporte,
+        idItem: req.params.id_item
+      }, function (err, transporte) {
+        if (err) res.send(err);
+    
+        res.json({ message: "Material eliminado." });
+      });
+    });
 
   return apiRouter;
 };
